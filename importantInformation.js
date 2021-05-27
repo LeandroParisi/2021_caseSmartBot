4cee2a49207d068803a35cd4cc8543b2c68b431a
LeandroParisi <lepcbelisario@gmail.com>
2021-05-27 19:06:23 -0300

    setup: imports logo to project assets

---
+++ importantInformation.js
@@ -0,0 +1,6 @@
+const simulado_real = `Esse modo tem como função, filtrar os robôs que serão exibidos, de acordo com o modo selecionado, caso esteja selecionado o "Simulado", serão exibidos os robôs que executam em ambiente simulado, e no caso de "Real", os robôs que executam em ambiente real.
+Essa condição de escolha, é para a criação do robô, e o modo nesse caso, tem a função de definir para qual ambiente o robô será criado. Caso seja criado no "Simulado", o usuário poderá escolher se ele gostaria de utilizar o simulador Otimista ou Pessimista, caso seja criado no "Real", ele deverá escolher uma corretora para operar o robô.
+
+Se a dúvida permanecer, é só me falar!
+
+Abraços,`
