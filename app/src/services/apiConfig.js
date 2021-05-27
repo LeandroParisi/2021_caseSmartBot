bb4dcbd04fee08db54f9f12e421d5941e5bf393e
LeandroParisi <lepcbelisario@gmail.com>
2021-05-27 19:35:49 -0300

    setup: creates base default fetcher and API endpoints

---
+++ app/src/services/apiConfig.js
@@ -0,0 +1,28 @@
+export const url = 'https://api-front-test.k8s.smarttbot.com/api/v1';
+
+export const endpoints = {
+  listRobots: () => ({
+    method: 'GET',
+    endpoint: '/robot',
+  }),
+  createRobot: () => ({
+    method: 'POST',
+    endpoint: '/robot',
+  }),
+  robotOverview: () => ({
+    method: 'GET',
+    endpoint: '/robot/overview',
+  }),
+  findRobotById: (id) => ({
+    method: 'GET',
+    endpoint: `/robot/${id}`,
+  }),
+  startRobot: (id) => ({
+    method: 'PUT',
+    endpoint: `/robot/${id}/start`,
+  }),
+  stopRobot: (id) => ({
+    method: 'PUT',
+    endpoint: `/robot/${id}/stop`,
+  }),
+};
